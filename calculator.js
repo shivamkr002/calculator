66bee7caed86b5f9493e3782ffa5477c198e128f
shivam kumar <shivamkr2587@gmail.com>
2024-10-21 17:04:54 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,30 @@
+    //1st question---->
+
+// const mycolor =["Red", "Green" ,"Black","white"]
+// let text =  mycolor.toString("Red");
+// console.log(text)
+      
+      //2nd question---->
+
+//  const mycolor =["Red", "Green" ,"Black","white"]
+//  let text =  mycolor.includes("Red");
+//  console.log(text)  
+
+   //3rd question--->
+
+//    const mycolor =["Red", "Green" ,"Black","white"]
+//    const newcolor=["blue","yellow"]
+//    const text=mycolor.concat(newcolor);
+//    console.log(text)
+
+    //4th-question--->
+
+    // const fruits = ["Banana", "Orange", "Apple", "Mango"]
+    // let text=fruits.sort();
+    // console.log(text)  
+    
+    //5th-question--->
+
+    // const mycolor =["Red", "Green" ,"Black","white"]
+    // let text=mycolor.reverse()
+    // console.log(text)  
